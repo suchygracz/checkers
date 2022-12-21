a3d28bcac463b5c0fcc3fed6a5aba2538551ed52
Szymon <szjaniak1@gmail.com>
2022-12-21 20:26:08 +0100

    zmiana niektorych nazw zmiennych i funkcji na jednolity jezyk

---
+++ src/main/java/Piece.java
@@ -0,0 +1,48 @@
+public class Piece {
+    int ID;
+    String color;
+    String type;
+    int posX, posY;
+    public Piece(String color, String type, int posX, int posY) {
+        this.color = color;
+        this.type = type;
+        this.posX = posX;
+        this.posY = posY;
+    }
+    public void movePiece(Piece piece, int desiredPosX, int desiredPosY){
+        piece.setPosX(desiredPosX);
+        piece.setPosY(desiredPosY);
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
--- src/main/java/board.java
+++
@@ -1,5 +0,0 @@
-public class board {
-    public board(int rows, int columns,){
-57
-    }
-}
