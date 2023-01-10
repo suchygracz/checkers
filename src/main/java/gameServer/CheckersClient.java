fb7401facda8c6b367ad5f792f039a01bae49295
Wiktor <such.wik@gmail.com>
2023-01-10 20:32:57 +0100

    klasa klienta wraz z planszą do gry i guzikami do wyboru rodzaju gry

--- src/main/java/gameServer/CheckersClient.java
+++ src/main/java/gameServer/CheckersClient.java
@@ -1,10 +1,15 @@
 package gameServer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class CheckersClient extends Application {
     public static void main(String[] args) {
@@ -13,7 +18,26 @@ public class CheckersClient extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        VBox root = new VBox();
         GridPane board = new GridPane();
+        board.setPadding(new Insets(0,0,0,0));
+
+        HBox buttons = new HBox();
+        buttons.setPadding(new Insets(10, 10, 10, 10)); // top, right, bottom, left
+        buttons.setSpacing(10);
+
+        // Create the buttons
+        Button russianGameButton = new Button("Russian Game");
+        Button turkishGameButton = new Button("Turkish Game");
+        Button englishGameButton = new Button("English Game");
+        russianGameButton.setPrefWidth(120);
+        turkishGameButton.setPrefWidth(120);
+        englishGameButton.setPrefWidth(120);
+
+
+        // Add the buttons to the container
+        buttons.getChildren().addAll(russianGameButton, turkishGameButton, englishGameButton);
+        // Add the button container to the top of the board
 
         // Add the tiles to the board
         for (int row = 0; row < 8; row++) {
@@ -33,8 +57,12 @@ public class CheckersClient extends Application {
             }
         }
 
-        Scene scene = new Scene(board);
-        primaryStage.setScene(scene);
+        // Add the button container to the top of the board
+        root.getChildren().addAll(buttons, board);
+
+
+        Scene scene = new Scene(root);//root=scene
+        primaryStage.setScene(scene);//root=scene
         primaryStage.show();
     }
 }
