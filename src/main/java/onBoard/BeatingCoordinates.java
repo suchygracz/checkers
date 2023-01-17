16eda9922efdf1999a7a7809a389f0dcbc368067
Wiktor <such.wik@gmail.com>
2023-01-17 18:38:21 +0100

    usunięcie klas nadmiarowych używanych w procesie rozwijania projektu jednak już w tym momencie nie potzebnych

--- src/main/java/onBoard/BeatingCoordinates.java
+++ src/main/java/onBoard/BeatingCoordinates.java
@@ -8,8 +8,8 @@ public class BeatingCoordinates {
     public final Pair<Integer, Integer> lostPiece;
     public BeatingCoordinates(final boolean ifTrue, final Pair<Integer, Integer> nextPos, Pair<Integer, Integer> lostPiece)
     {
-        this.ifTrue = ifTrue;
-        this.nextPos = nextPos;
-        this.lostPiece = lostPiece;
+        this.ifTrue = ifTrue;//czy jest bicie
+        this.nextPos = nextPos;//gdzie sie trzeba ruszyć zeby zbic
+        this.lostPiece = lostPiece;//koordynaty zbitego
     }
 }
